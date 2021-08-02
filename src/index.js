cb4fdd33d898e5c942ea455809d5fd10cf79e68d
Thomas Lance McCluskey <40124399+lancemccluskey@users.noreply.github.com>
2021-08-02 08:32:18 -0500

    feat: updating release please and lint (#9)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({
-  logger: true
-})
+  logger: true,
+});
 
 // Declare a route
 fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
-})
+  reply.send({ hello: 'world' });
+});
 
 // Run the server!
 fastify.listen(3000, function (err, address) {
   if (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
-  fastify.log.info(`server listening on ${address}`)
-})
+  fastify.log.info(`server listening on ${address}`);
+});

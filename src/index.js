74a176bf0fec531897c5149ef24b889df988ccfb
Thomas Lance McCluskey <40124399+lancemccluskey@users.noreply.github.com>
2021-08-02 10:31:24 -0500

    feat: testing conditional wkflow step (#23)

--- src/index.js
+++ src/index.js
@@ -26,5 +26,5 @@ fastify.listen(3000, function (err, address) {
     fastify.log.error(err);
     process.exit(1);
   }
-  fastify.log.info(`server listening on ${address}`);
+  fastify.log.info(`Server listening on ${address}`);
 });
